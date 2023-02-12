af96c00b5f45d9721c6fc057acfa94896d041d53
jennereh <jenner@jennereh.com>
2023-02-12 15:21:18 -0800

    Handle basic args

--- xlsx2json/xlsx2json.py
+++ xlsx2json/xlsx2json.py
@@ -8,11 +8,28 @@ def main():
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', type=str, help='Name of an input file')
-    parser.add_argument('-o', '--output', type=str, help='Name of an output file')
-    parser.add_argument('-s', '--schema', dest='schema_arg', type=str, help='Specify a reference schema')
-    parser.add_argument('-f', '--force', dest='force_arg', type=str, help='Force an overwrite or skip conflicts')
-    parser.add_argument('-db', '--existing-db', dest='existing_db_arg', type=str, help='Merge with this existing db.json file')
+    parser.add_argument('-i', '--input',
+                        dest='input_file',
+                        type=str,
+                        help='Name of an input file',
+                        required=True)
+    parser.add_argument('-o', '--output',
+                        dest='output_file',
+                        type=str,
+                        help='Name of an output file',
+                        required=True)
+    parser.add_argument('-s', '--schema',
+                        dest='schema_filename',
+                        type=str,
+                        help='Specify a reference schema')
+    parser.add_argument('-f', '--force',
+                        dest='force',
+                        type=str,
+                        help='Force an overwrite or skip conflicts')
+    parser.add_argument('-db', '--existing-db',
+                        dest='existing_db_file',
+                        type=str,
+                        help='Merge with this existing db.json file')
     args = parser.parse_args()
 
     print(args)
