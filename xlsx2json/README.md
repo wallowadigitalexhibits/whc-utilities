b4f7fc7a96fe71a413ddf508fcfb7e9d936669cf
jennereh <jenner@jennereh.com>
2023-02-12 17:37:38 -0800

    Stash before conversion to js

--- xlsx2json/README.md
+++ xlsx2json/README.md
@@ -11,19 +11,25 @@ sudo apt install python3-pandas
 ./xlsx2json.py --schema=archive-app.json --existing-db=db.json --force=skip in.xlsx out.json
 ```
 
-The script takes `.xlsx` and `.ods` files, and outputs a `.json` file containing a list of node objects. 
+**Required Arguments**
 
-Each sheet in the spreadsheet must have the type of node, which must be one of the following: `Item`, `Keyword`, `ItemAcquisitionRecord`, `List`, `User`. You can see the `nodes.json` file for the resulting schema.
+The script takes a single `.xlsx` or `.ods` file, and outputs a `.json` file containing a list of node objects. 
+
+This script will work for any schema. It requires a reference JSON database file containing, at minimum, label nodes corresponding to the names of each sheet in the input spreadsheet. 
+
+For working with the Wallowa History Center's Archive App, the allowable node labels are: `Item`, `Keyword`, `ItemAcquisitionRecord`, `List`, and `User`. You can see the `archive-app.json` file for the bare minimum database file required.
+
+To import items into a larger existing JSON database with many existing nodes, the label nodes must exist. The script will use those label nodes to create and add object nodes to the existing database. 
 
 <img src="img/sheets.jpg">
 
 The spreadsheet must contain a header row. The first item in the header row must be the `id` to be stored in `core_props`. It will merge whatever columns exist into the reference schema node, leaving non-specified entries blank or in their default value. This means you can have a sheet called `Item` that only has `id`, `strTitle`, and `strDescription`, but still end up with a compliant `db.json` of nodes where the only values known are the title and description. 
 
-This is also useful for merging into an existing database with the `--existing-db=file.json` flag. The script will attempt to add the new nodes to the existing database. If there are clashes with a node's id already existing, the script will print the list of clashing nodes for your inspection. 
+If there are clashes with a node's id already existing, the script will print the list of clashing nodes for your inspection. 
 
 If you then add a `--force=override` or `--force=skip` flag, the program will proceed and create the new file. This is handy for adding a single property to an existing database.
 
-If you only have an Item sheet without a Keyword sheet, the `--extract-keywords` flag will create `Keyword` nodes for you.
+Every column which is prefixed by `rel`, i.e. a related node, must have its header cell include the destination type in a bracket. All related nodes headers need to have the related node type in brackets. If you have a sheet which references Item sheet without a Keyword sheet, but the `--extract-keywords` flag will create `Keyword` nodes for you.
 
 The intent is to easily incorporate contributions from less technically-savvy folks into the master database for viewing in the Archive App. 
 
--- xlsx2json/xlsx2json.py
+++ xlsx2json/xlsx2json.py
@@ -1,9 +1,68 @@
 #!/usr/bin/python
 
-import sys, pandas, argparse
+import sys, json, pandas, argparse, pathlib
 
 def main():
     print('xlsx2json')
+
+    # set global vars and flags
+    dryRunFlag = False
+    referenceDb = []
+    referenceDbLabels = []
+
+    # check and handle the force flag
+
+    ##############################
+    ### read the database file ###
+    ##############################
+    # - get the list of available node labels
+
+    pDb = pathlib.Path(args.existing_db_file)
+    try:
+        with pDb.open() as fDb:
+            referenceDb = json.load(fDb)
+            print('loading the database file')
+            if referenceDb:
+                for node in referenceDb:
+                    referenceDbLabels.append(node['core_props']['label'])
+                referenceDbLabels = set(referenceDbLabels)
+                print(referenceDbLabels)
+            else:
+                print('ERROR: database file is empty')
+
+    except OSError:
+        print('ERROR: database file does not exist')
+
+    ####################################
+    ### read in the spreadsheet file ###
+    ####################################
+    # - identify all the sheets
+    #       must have node labels or error & exit
+    # - does each sheet have a header row?
+    #       must have a header row or error & exit
+    # - is the first column 'id'?
+    #       must have first column as 'id' or error & exit
+    # - do all the column header names match the schema?
+    #       must match or is force arg present?
+    #           if not, enter dry-run mode with error message
+    #           if force=override or force=skip, ignore these columns
+
+    pInput = pathlib.Path(args.input_file)
+    try:
+        with pInput.open() as fInput:
+            print('input file exists')
+
+            # identify all the sheets
+
+
+    except OSError:
+        print('ERROR: input file does not exist')
+
+    ##############################
+    ### build the new database ###
+    ##############################
+    #
+
 
 if __name__ == '__main__':
 
@@ -18,10 +77,10 @@ if __name__ == '__main__':
                         type=str,
                         help='Name of an output file',
                         required=True)
-    parser.add_argument('-s', '--schema',
-                        dest='schema_filename',
+    parser.add_argument('-dry', '--dry-run',
+                        dest='force',
                         type=str,
-                        help='Specify a reference schema')
+                        help='Force an overwrite or skip conflicts')
     parser.add_argument('-f', '--force',
                         dest='force',
                         type=str,
@@ -29,7 +88,8 @@ if __name__ == '__main__':
     parser.add_argument('-db', '--existing-db',
                         dest='existing_db_file',
                         type=str,
-                        help='Merge with this existing db.json file')
+                        help='Existing db with (at minimum) label nodes defining each sheet to import',
+                        required=True)
     args = parser.parse_args()
 
     print(args)
